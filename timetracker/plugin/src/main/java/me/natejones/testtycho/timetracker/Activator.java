48777acd94c72a9f88a995bde3583a9655405f7c
Nate Jones <n8jones@gmail.com>
2014-12-21 15:12:53 -0500

    Creating a copy of the minimal project in the timetracker folder.

---
+++ timetracker/plugin/src/main/java/me/natejones/testtycho/timetracker/Activator.java
@@ -0,0 +1,49 @@
+package me.natejones.testtycho.timetracker;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "TestEclipsePlugin01"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+}
---
+++ timetracker/plugin/src/main/java/me/natejones/testtycho/timetracker/View.java
@@ -0,0 +1,18 @@
+package me.natejones.testtycho.timetracker;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.SWT;
+import org.eclipse.ui.part.ViewPart;
+
+public class View extends ViewPart {
+	Label label;
+
+	public void createPartControl(Composite parent) {
+		label = new Label(parent, SWT.WRAP);
+		label.setText("Hello World");
+	}
+
+	public void setFocus() {
+	}
+}
